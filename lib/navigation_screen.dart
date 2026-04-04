f56740dfb696764d0d5b11bbd69248229d867e69
Abdallah Hussien <abdallah.hussien.muhammad@gmail.com>
2026-04-04 15:37:01 +0200

    Remove unused imports and make tab children const

--- lib/navigation_screen.dart
+++ lib/navigation_screen.dart
@@ -1,10 +1,7 @@
-import 'package:e_commerce/features/home/logic/cubit/home_cubit.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-import 'core/di/dependancy_injection.dart';
 import 'core/helpers/spacing.dart';
 import 'core/theme/color_manager.dart';
 import 'features/cart/account/account_screen.dart';
@@ -35,6 +32,7 @@ class _NavigationScreenState extends State<NavigationScreen> {
 
   @override
   Widget build(BuildContext context) {
+    // ignore: deprecated_member_use
     return WillPopScope(
       onWillPop: _onWillPop,
       child: Scaffold(
@@ -87,7 +85,7 @@ class _NavigationScreenState extends State<NavigationScreen> {
         ),
         body: IndexedStack(
           index: _currentIndex, // ← Keeps all tabs mounted
-          children: [HomeScreen(), CartScreen(), AccountScreen()],
+          children:const [HomeScreen(), CartScreen(), AccountScreen()],
         ),
       ),
     );
